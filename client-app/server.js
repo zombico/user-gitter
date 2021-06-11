6bb01d194af07079d0b479d35ba701a15aa3d38c
zombico <franz@ombi.co>
2021-06-11 08:38:22 -0400

    initial committy

---
+++ client-app/server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const path = require("path");
+
+app.use('/', express.static(
+  path.join(__dirname, '../build'))
+)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'))
+})
+
+module.exports = app;
